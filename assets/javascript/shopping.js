174e9b4754fa035181b6700483da844700bfeb1a
Rishi-atgondan <rishijeeva13@gmail.com>
2021-12-15 19:51:13 +0530

    modified

--- assets/javascript/shopping.js
+++ assets/javascript/shopping.js
@@ -6,13 +6,27 @@ function clear2() {
     document.getElementById("div_2").style.display = "none";
 }
 
+function action1() {
+    window.open("https://play.google.com/store/apps/details?id=com.pubg.imobile", "_blank")
+}
 
+function action2() {
+    window.open("https://apps.apple.com/in/app/battlegrounds-mobile-india/id1526436837", "_blank")
+
+}
 
 let noOfUc;
 
 function display(x) {
-    noOfUc = x;
-    return noOfUc;
+    let input = document.getElementById("chr_id").value;
+    if (input == "") {
+        document.getElementById("error").innerHTML = "*Please fill user id";
+    }
+    else if (input != "") {
+        noOfUc = x;
+        return noOfUc;
+    }
+
 }
 
 let userList = [];
@@ -21,21 +35,37 @@ function submitHandler() {
     const characterId = document.getElementById("chr_id").value;
 
     if (noOfUc == null) {
-        alert("Please add No of UC");
+        alert("Please add No Of UC");
     } else {
-        const obj = {
+        const objectUC = {
             "user ID": characterId,
             "UC": noOfUc
         }
-        userList.push(obj);
+        userList.push(objectUC);
 
         let detailsInString = JSON.stringify(userList);
         localStorage.setItem("UC purchased", detailsInString);
         alert("Thank you for purchasing UC");
+        window.location.reload();
     }
-
-
 }
+
+function getAll() {
+    const arrayToString = localStorage.getItem("UC purchased");
+    let inArray = [];
+    if (arrayToString) {
+        inArray = JSON.parse(arrayToString);
+    } else {
+        inArray = [];
+    }
+    return inArray;
+}
+function pageOnLoadHandler() {
+    const all = getAll();
+    userList = all;
+}
+
+pageOnLoadHandler();
 
 
 
