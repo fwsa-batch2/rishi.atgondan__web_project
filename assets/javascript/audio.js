23fdfe48a3eaee33d94f8bbca148cbf78c3077d4
Rishi-atgondan <rishijeeva13@gmail.com>
2022-01-10 18:07:31 +0530

    cleared bugs in index page

---
+++ assets/javascript/audio.js
@@ -0,0 +1,2 @@
+let audio = new Audio('./../../assets/images/duck.mp3');
+audio.play();
---
+++ assets/javascript/game.js
@@ -0,0 +1,98 @@
+// function alert() {
+//     prompt.Audio.play('../../assets/images/duck.mp3')
+// }
+function start() {
+    document.getElementById("").style.visibility = "hidden"
+}
+
+function play(x) {
+    window.location.href = "./../../pages/game.html"
+}
+
+function duck1(x) {
+    document.getElementById('img1').style.visibility = "hidden";
+    let audio = new Audio('./../../assets/images/gunsound.mp3');
+    audio.play();
+}
+function duck2() {
+    document.getElementById('img2').style.visibility = "hidden";
+    let audio = new Audio('./../../assets/images/gunsound.mp3');
+    audio.play();
+}
+function duck3() {
+    document.getElementById('img3').style.visibility = "hidden";
+    let audio = new Audio('./../../assets/images/gunsound.mp3');
+    audio.play();
+}
+function duck4() {
+    document.getElementById('img4').style.visibility = "hidden";
+    let audio = new Audio('./../../assets/images/gunsound.mp3');
+    audio.play();
+}
+function duck5() {
+    document.getElementById('img5').style.visibility = "hidden";
+    let audio = new Audio('./../../assets/images/gunsound.mp3');
+    audio.play();
+}
+function duck6() {
+    document.getElementById('img6').style.visibility = "hidden";
+    let audio = new Audio('./../../assets/images/gunsound.mp3');
+    audio.play();
+}
+function duck7() {
+    document.getElementById('img7').style.visibility = "hidden";
+    let audio = new Audio('./../../assets/images/gunsound.mp3');
+    audio.play();
+}
+function duck8() {
+    document.getElementById('img8').style.visibility = "hidden";
+    let audio = new Audio('./../../assets/images/gunsound.mp3');
+    audio.play();
+}
+function duck9() {
+    document.getElementById('img9').style.visibility = "hidden";
+    let audio = new Audio('./../../assets/images/gunsound.mp3');
+    audio.play();
+}
+function duck10() {
+    document.getElementById('img10').style.visibility = "hidden";
+    let audio = new Audio('./../../assets/images/gunsound.mp3');
+    audio.play();
+}
+function duck11() {
+    document.getElementById('img11').style.visibility = "hidden";
+    let audio = new Audio('./../../assets/images/gunsound.mp3');
+    audio.play();
+}
+function duck12() {
+    document.getElementById('img12').style.visibility = "hidden";
+    let audio = new Audio('./../../assets/images/gunsound.mp3');
+    audio.play();
+}
+function duck13() {
+    document.getElementById('img13').style.visibility = "hidden";
+    let audio = new Audio('./../../assets/images/gunsound.mp3');
+    audio.play();
+}
+function duck14() {
+    document.getElementById('img14').style.visibility = "hidden";
+    let audio = new Audio('./../../assets/images/gunsound.mp3');
+    audio.play();
+}
+function duck15() {
+    document.getElementById('img15').style.visibility = "hidden";
+    let audio = new Audio('./../../assets/images/gunsound.mp3');
+    audio.play();
+}
+
+
+
+
+
+
+
+
+
+
+
+
