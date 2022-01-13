01e0f1ea7d453fa3e6a81c683602ce5b624f6d89
Rishi-atgondan <rishijeeva13@gmail.com>
2022-01-13 16:40:49 +0530

    created a game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,86 +1,322 @@
-function start() {
-    document.getElementById("").style.visibility = "hidden"
-}
 
 function play(x) {
     window.location.href = "./../../pages/game.html"
 }
 
-function duck1(x) {
+let str = "0";
+
+function teddy1(x) {
     document.getElementById('img1').style.visibility = "hidden";
     let audio = new Audio('./../../assets/images/gunsound.mp3');
     audio.play();
+    let num = parseFloat(str) + 100;
+    document.getElementById("points").innerHTML = num;
+    if (num < 1600 && num > 0) {
+        str = JSON.stringify(num);
+    } else if (num == 1600) {
+        document.getElementById("winner1").style.display = "block";
+        document.getElementById("winner2").style.display = "block";
+        let audio1 = new Audio('./../../assets/images/claps.wav');
+        audio1.play();
+        let audio2 = new Audio('./../../assets/images/rocket.wav');
+        audio2.play();
+        document.getElementById('prize').style.display = "block";
+        document.getElementById('collect').style.display = "block";
+    }
 }
-function duck2() {
+function teddy2() {
     document.getElementById('img2').style.visibility = "hidden";
     let audio = new Audio('./../../assets/images/gunsound.mp3');
     audio.play();
+    let num = parseFloat(str) + 100;
+    document.getElementById("points").innerHTML = num;
+    if (num < 1600 && num > 0) {
+        str = JSON.stringify(num);
+    } else if (num == 1600) {
+        document.getElementById("winner1").style.display = "block";
+        document.getElementById("winner2").style.display = "block";
+        let audio1 = new Audio('./../../assets/images/claps.wav');
+        audio1.play();
+        let audio2 = new Audio('./../../assets/images/rocket.wav');
+        audio2.play();
+        document.getElementById('prize').style.display = "block";
+        document.getElementById('collect').style.display = "block";
+    }
 }
-function duck3() {
+function teddy3() {
     document.getElementById('img3').style.visibility = "hidden";
     let audio = new Audio('./../../assets/images/gunsound.mp3');
     audio.play();
+    let num = parseFloat(str) + 100;
+    document.getElementById("points").innerHTML = num;
+    if (num < 1600 && num > 0) {
+        str = JSON.stringify(num);
+    } else if (num == 1600) {
+        document.getElementById("winner1").style.display = "block";
+        document.getElementById("winner2").style.display = "block";
+        let audio1 = new Audio('./../../assets/images/claps.wav');
+        audio1.play();
+        let audio2 = new Audio('./../../assets/images/rocket.wav');
+        audio2.play();
+        document.getElementById('prize').style.display = "block";
+        document.getElementById('collect').style.display = "block";
+    }
 }
-function duck4() {
+function teddy4() {
     document.getElementById('img4').style.visibility = "hidden";
     let audio = new Audio('./../../assets/images/gunsound.mp3');
     audio.play();
+    let num = parseFloat(str) + 100;
+    document.getElementById("points").innerHTML = num;
+    if (num < 1600 && num > 0) {
+        str = JSON.stringify(num);
+    } else if (num == 1600) {
+        document.getElementById("winner1").style.display = "block";
+        document.getElementById("winner2").style.display = "block";
+        let audio1 = new Audio('./../../assets/images/claps.wav');
+        audio1.play();
+        let audio2 = new Audio('./../../assets/images/rocket.wav');
+        audio2.play();
+        document.getElementById('prize').style.display = "block";
+        document.getElementById('collect').style.display = "block";
+    }
 }
-function duck5() {
+function teddy5() {
     document.getElementById('img5').style.visibility = "hidden";
     let audio = new Audio('./../../assets/images/gunsound.mp3');
     audio.play();
+    let num = parseFloat(str) + 100;
+    document.getElementById("points").innerHTML = num;
+    if (num < 1600 && num > 0) {
+        str = JSON.stringify(num);
+    } else if (num == 1600) {
+        document.getElementById("winner1").style.display = "block";
+        document.getElementById("winner2").style.display = "block";
+        let audio1 = new Audio('./../../assets/images/claps.wav');
+        audio1.play();
+        let audio2 = new Audio('./../../assets/images/rocket.wav');
+        audio2.play();
+        document.getElementById('prize').style.display = "block";
+        document.getElementById('collect').style.display = "block";
+    }
 }
-function duck6() {
+function teddy6() {
     document.getElementById('img6').style.visibility = "hidden";
     let audio = new Audio('./../../assets/images/gunsound.mp3');
     audio.play();
+    let num = parseFloat(str) + 100;
+    document.getElementById("points").innerHTML = num;
+    if (num < 1600 && num > 0) {
+        str = JSON.stringify(num);
+    } else if (num == 1600) {
+        document.getElementById("winner1").style.display = "block";
+        document.getElementById("winner2").style.display = "block";
+        let audio1 = new Audio('./../../assets/images/claps.wav');
+        audio1.play();
+        let audio2 = new Audio('./../../assets/images/rocket.wav');
+        audio2.play();
+        document.getElementById('prize').style.display = "block";
+        document.getElementById('collect').style.display = "block";
+    }
 }
-function duck7() {
+function teddy7() {
     document.getElementById('img7').style.visibility = "hidden";
     let audio = new Audio('./../../assets/images/gunsound.mp3');
     audio.play();
+    let num = parseFloat(str) + 100;
+    document.getElementById("points").innerHTML = num;
+    if (num < 1600 && num > 0) {
+        str = JSON.stringify(num);
+    } else if (num == 1600) {
+        document.getElementById("winner1").style.display = "block";
+        document.getElementById("winner2").style.display = "block";
+        let audio1 = new Audio('./../../assets/images/claps.wav');
+        audio1.play();
+        let audio2 = new Audio('./../../assets/images/rocket.wav');
+        audio2.play();
+        document.getElementById('prize').style.display = "block";
+        document.getElementById('collect').style.display = "block";
+    }
 }
-function duck8() {
+function teddy8() {
     document.getElementById('img8').style.visibility = "hidden";
     let audio = new Audio('./../../assets/images/gunsound.mp3');
     audio.play();
+    let num = parseFloat(str) + 100;
+    document.getElementById("points").innerHTML = num;
+    if (num < 1600 && num > 0) {
+        str = JSON.stringify(num);
+    } else if (num == 1600) {
+        document.getElementById("winner1").style.display = "block";
+        document.getElementById("winner2").style.display = "block";
+        let audio1 = new Audio('./../../assets/images/claps.wav');
+        audio1.play();
+        let audio2 = new Audio('./../../assets/images/rocket.wav');
+        audio2.play();
+        document.getElementById('prize').style.display = "block";
+        document.getElementById('collect').style.display = "block";
+    }
 }
-function duck9() {
+function teddy9() {
     document.getElementById('img9').style.visibility = "hidden";
     let audio = new Audio('./../../assets/images/gunsound.mp3');
     audio.play();
+    let num = parseFloat(str) + 100;
+    document.getElementById("points").innerHTML = num;
+    if (num < 1600 && num > 0) {
+        str = JSON.stringify(num);
+    } else if (num == 1600) {
+        document.getElementById("winner1").style.display = "block";
+        document.getElementById("winner2").style.display = "block";
+        let audio1 = new Audio('./../../assets/images/claps.wav');
+        audio1.play();
+        let audio2 = new Audio('./../../assets/images/rocket.wav');
+        audio2.play();
+        document.getElementById('prize').style.display = "block";
+        document.getElementById('collect').style.display = "block";
+    }
 }
-function duck10() {
+function teddy10() {
     document.getElementById('img10').style.visibility = "hidden";
     let audio = new Audio('./../../assets/images/gunsound.mp3');
     audio.play();
+    let num = parseFloat(str) + 100;
+    document.getElementById("points").innerHTML = num;
+    if (num < 1600 && num > 0) {
+        str = JSON.stringify(num);
+    } else if (num == 1600) {
+        document.getElementById("winner1").style.display = "block";
+        document.getElementById("winner2").style.display = "block";
+        let audio1 = new Audio('./../../assets/images/claps.wav');
+        audio1.play();
+        let audio2 = new Audio('./../../assets/images/rocket.wav');
+        audio2.play();
+        document.getElementById('prize').style.display = "block";
+        document.getElementById('collect').style.display = "block";
+
+    }
 }
-function duck11() {
+function teddy11() {
     document.getElementById('img11').style.visibility = "hidden";
     let audio = new Audio('./../../assets/images/gunsound.mp3');
     audio.play();
+    let num = parseFloat(str) + 100;
+    document.getElementById("points").innerHTML = num;
+    if (num < 1600 && num > 0) {
+        str = JSON.stringify(num);
+    } else if (num == 1600) {
+        document.getElementById("winner1").style.display = "block";
+        document.getElementById("winner2").style.display = "block";
+        let audio1 = new Audio('./../../assets/images/claps.wav');
+        audio1.play();
+        let audio2 = new Audio('./../../assets/images/rocket.wav');
+        audio2.play();
+        document.getElementById('prize').style.display = "block";
+        document.getElementById('collect').style.display = "block";
+    }
 }
-function duck12() {
+function teddy12() {
     document.getElementById('img12').style.visibility = "hidden";
     let audio = new Audio('./../../assets/images/gunsound.mp3');
     audio.play();
+    let num = parseFloat(str) + 100;
+    document.getElementById("points").innerHTML = num;
+    if (num < 1600 && num > 0) {
+        str = JSON.stringify(num);
+    } else if (num == 1600) {
+        document.getElementById("winner1").style.display = "block";
+        document.getElementById("winner2").style.display = "block";
+        let audio1 = new Audio('./../../assets/images/claps.wav');
+        audio1.play();
+        let audio2 = new Audio('./../../assets/images/rocket.wav');
+        audio2.play();
+        document.getElementById('prize').style.display = "block";
+        document.getElementById('collect').style.display = "block";
+    }
 }
-function duck13() {
+function teddy13() {
     document.getElementById('img13').style.visibility = "hidden";
     let audio = new Audio('./../../assets/images/gunsound.mp3');
     audio.play();
+    let num = parseFloat(str) + 100;
+    document.getElementById("points").innerHTML = num;
+    if (num < 1600 && num > 0) {
+        str = JSON.stringify(num);
+    } else if (num == 1600) {
+        document.getElementById("winner1").style.display = "block";
+        document.getElementById("winner2").style.display = "block";
+        let audio1 = new Audio('./../../assets/images/claps.wav');
+        audio1.play();
+        let audio2 = new Audio('./../../assets/images/rocket.wav');
+        audio2.play();
+        document.getElementById('prize').style.display = "block";
+        document.getElementById('collect').style.display = "block";
+    }
+
 }
-function duck14() {
+function teddy14() {
     document.getElementById('img14').style.visibility = "hidden";
     let audio = new Audio('./../../assets/images/gunsound.mp3');
     audio.play();
+    let num = parseFloat(str) + 100;
+    document.getElementById("points").innerHTML = num;
+    if (num < 1600 && num > 0) {
+        str = JSON.stringify(num);
+    } else if (num == 1600) {
+        document.getElementById("winner1").style.display = "block";
+        document.getElementById("winner2").style.display = "block";
+        let audio1 = new Audio('./../../assets/images/claps.wav');
+        audio1.play();
+        let audio2 = new Audio('./../../assets/images/rocket.wav');
+        audio2.play();
+        document.getElementById('prize').style.display = "block";
+        document.getElementById('collect').style.display = "block";
+    }
 }
-function duck15() {
+function teddy15() {
     document.getElementById('img15').style.visibility = "hidden";
     let audio = new Audio('./../../assets/images/gunsound.mp3');
     audio.play();
+    let num = parseFloat(str) + 100;
+    document.getElementById("points").innerHTML = num;
+    if (num < 1600 && num > 0) {
+        str = JSON.stringify(num);
+    } else if (num == 1600) {
+        document.getElementById("winner1").style.display = "block";
+        document.getElementById("winner2").style.display = "block";
+        let audio1 = new Audio('./../../assets/images/claps.wav');
+        audio1.play();
+        let audio2 = new Audio('./../../assets/images/rocket.wav');
+        audio2.play();
+        document.getElementById('prize').style.display = "block";
+        document.getElementById('collect').style.display = "block";
+    }
 }
+function teddy16() {
+    document.getElementById('img16').style.visibility = "hidden";
+    let audio = new Audio('./../../assets/images/gunsound.mp3');
+    audio.play();
+    let num = parseFloat(str) + 100;
+    document.getElementById("points").innerHTML = num;
+    if (num < 1600 && num > 0) {
+        str = JSON.stringify(num);
+    } else if (num == 1600) {
+        document.getElementById("winner1").style.display = "block";
+        document.getElementById("winner2").style.display = "block";
+        let audio1 = new Audio('./../../assets/images/claps.wav');
+        audio1.play();
+        let audio2 = new Audio('./../../assets/images/rocket.wav');
+        audio2.play();
+        document.getElementById('prize').style.display = "block";
+        document.getElementById('collect').style.display = "block";
+    }
+}
+
+function redirect(x) {
+    if (x == "collect")
+        window.location.href = "./../../index.html"
+}
+redirect(x);
 
 
 
