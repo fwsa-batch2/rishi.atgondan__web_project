e1aaee5a62a687aa17e69439f10f6eb46e197e5d
Rishi <93571045+Rishi-atgondan@users.noreply.github.com>
2022-02-28 15:33:55 +0530

    Delete assets/javascript directory

--- assets/javascript/audio.js
+++
@@ -1,3 +0,0 @@
-let audio = new Audio('/rishi.atgondan__web_project/assets/images/duck.mp3');
-audio.play();
-
--- assets/javascript/cartlink.js
+++
@@ -1,4 +0,0 @@
-function cart() {
-    window.open("/rishi.atgondan__web_project//pages/shopping.html", "_blank")
-}
-
